563ff66378287d71d615a0f0b7f633958044924f
unknown <zinedinmostafiz@gmail.com>
2025-11-03 19:53:36 +0600

    23-54007-3

---
+++ project/calculator/calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+do
+{
+    int num1 = Console.ReadLine();
+    int num2 = Console.ReadLine();
+    int addition = num1 + num2;
+    int subtraction = num1 - num2;
+    int multiplication = num1 * num2;
+    int division = num1 / num2;
+    Console.WriteLine("==Simple Calculator==");
+    Console.WriteLine("1.addition" + addition);
+    Console.WriteLine("2.Subtraction" + subtraction);
+    Console.WriteLine("3.multiplication" + multiplication);
+    Console.WriteLine("4.division" + division);
+    string menu = Console.ReadLine();
+    
+   
+}while(bool true);
+if(bool false)
+        {
+    Console.WriteLine("program end");
+}
