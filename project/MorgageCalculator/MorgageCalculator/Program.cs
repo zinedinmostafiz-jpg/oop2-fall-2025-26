280bc582cc2b24eed8606800944636bdc7c401f3
unknown <zinedinmostafiz@gmail.com>
2025-11-05 14:39:15 +0600

    23-54007-3

---
+++ project/MorgageCalculator/MorgageCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+long p = Console.ReadLine("Enter total amount:");
+long r = Console.ReadLine("Enter interest rate:");
+int n = Console.ReadLine("Enter number of payments:");
+
+long monthlyInterest = r / (100 * 12);
+int numberofPayment   = n * 12;
+long m = r * Math.Pow(1 + monthlyInterest, numberofPayment) / Math.Pow(1 + monthlyInterest, numberofPayment) - 1;
+Console.WriteLine("Total monthly morgage payment:"{ m});
+class main
+{
+    static void main()
+    {
+        cultureinfo bdculture = new cultureinfo("bn-bd");
+        Console.WriteLine("Total monthly morgage payment:"ToString("c"{ m}));
+        Console.WriteLine(ToString("c"{ p}));
+
+    }
+        
+
+
+}
+
--- project/calculator/calculator/obj/Release/net9.0/calculator.AssemblyInfo.cs
+++ project/calculator/calculator/obj/Release/net9.0/calculator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+563ff66378287d71d615a0f0b7f633958044924f")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
